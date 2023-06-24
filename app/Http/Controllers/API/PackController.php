79fe25e6b68ae8835bc7699cdea4219e14f23760
3cha9 <yoshq.xd@gmail.com>
2023-06-24 21:55:28 +0100

    new api documentation

--- app/Http/Controllers/API/PackController.php
+++ app/Http/Controllers/API/PackController.php
@@ -6,14 +6,27 @@ use App\Models\Pack;
 use App\Http\Resources\PackResource;
 use App\Http\Controllers\Controller;
 
-class PackController extends Controller
+class PackController extends Controller 
 {
-    
+    /**
+     * Show all packs
+     *
+     * 
+     *
+     * 
+     * */ 
     public function index()
     {
         return PackResource::collection(Pack::all());
     }
 
+    /**
+     * show a specific pack
+     *
+     * 
+     *
+     * 
+     * */ 
     public function show(Pack $pack)
     {
         return new PackResource($pack);
